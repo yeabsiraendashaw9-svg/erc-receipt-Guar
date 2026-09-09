113142f0e12146afc5456cc9ec6a5f197b6e0a98
yeabsiraendashaw9-svg <yeabsiraendashaw9@gmail.com>
2026-09-09 12:30:42 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'erca-receipt-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
